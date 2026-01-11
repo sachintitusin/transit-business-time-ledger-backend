ea69063c0cc3f75cfba5e058f09a78182696f7c5
Sachin Titus <sachintitusin@gmail.com>
2026-01-11 21:15:15 +0530

    feat(application): extend WorkPeriodRepository for read-side analytics

--- src/application/ports/WorkPeriodRepository.ts
+++ src/application/ports/WorkPeriodRepository.ts
@@ -4,5 +4,6 @@ import { DriverId, WorkPeriodId } from '../../domain/shared/types'
 export interface WorkPeriodRepository {
     findById(workPeriodId: WorkPeriodId): Promise<WorkPeriod | null>
     findOpenByDriver(driverId: DriverId): Promise<WorkPeriod | null>
+    findClosedByDriver(driverId: DriverId): Promise<WorkPeriod[]>
     save(workPeriod: WorkPeriod): Promise<void>
 }
