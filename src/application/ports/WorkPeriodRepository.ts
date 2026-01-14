6eae066f04bc17b9c06b4b0d7f28ff6a107cd3b6
Sachin Titus <sachintitusin@gmail.com>
2026-01-15 00:20:38 +0530

    chore: test helpers + middleware

--- src/application/ports/WorkPeriodRepository.ts
+++ src/application/ports/WorkPeriodRepository.ts
@@ -1,10 +1,16 @@
 import { WorkPeriod } from '../../domain/work/WorkPeriod'
 import { DriverId, WorkPeriodId } from '../../domain/shared/types'
+import { TimeRange } from '../../domain/shared/TimeRange'
 
 export interface WorkPeriodRepository {
     findById(workPeriodId: WorkPeriodId): Promise<WorkPeriod | null>
     findByDriver(driverId: DriverId): Promise<WorkPeriod[]>
     findOpenByDriver(driverId: DriverId): Promise<WorkPeriod | null>
     findClosedByDriver(driverId: DriverId): Promise<WorkPeriod[]>
+    findEffectiveOverlapping(
+        driverId: DriverId,
+        range: TimeRange,
+        excludeWorkId?: WorkPeriodId
+    ): Promise<WorkPeriod[]>
     save(workPeriod: WorkPeriod): Promise<void>
 }
